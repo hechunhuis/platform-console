d4679b08968e90023183a3073168860b0b0b1d03
何春辉 <hechunhui_email@163.com>
2023-02-07 17:56:13 +0800

    refactor 重构项目结构

---
+++ platform-common/common-core/src/main/java/com/platform/common/base/BaseEntity.java
@@ -0,0 +1,24 @@
+package com.platform.common.base;
+
+import java.io.Serializable;
+
+/**
+ * @ClassName BaseEntity
+ * @Description TODO
+ * @Author hechunhui
+ * @Date 2023/2/7 16:59
+ * @Version 1.0
+ */
+public class BaseEntity implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+//    @TableField(fill = FieldFill.INSERT)
+//    @JsonInclude(value = JsonInclude.Include.NON_NULL)
+//    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+//    private LocalDateTime createTime;
+//
+//    @TableField(fill = FieldFill.INSERT_UPDATE)
+//    @JsonInclude(value = JsonInclude.Include.NON_NULL)
+//    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+//    private LocalDateTime updateTime;
+}
